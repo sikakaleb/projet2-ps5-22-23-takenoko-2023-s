7db3c9e9f3cbcf84293fc4bd4e68e4c11f487157
Kaleb <dassou-ablam-kaleb.sika@etu-univ-cotedazur.fr>
2022-12-03 14:17:27 +0100

    Creation de la classe HexPlot : Parcelle

---
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/HexPlot.java
@@ -0,0 +1,17 @@
+package fr.cotedazur.univ.polytech.startingpoint;
+
+public class HexPlot {
+    int q;
+    int r;
+    int s;
+
+    public HexPlot(int q, int r, int s) {
+        this.q = q;
+        this.r = r;
+        this.s = s;
+    }
+
+
+
+
+}
