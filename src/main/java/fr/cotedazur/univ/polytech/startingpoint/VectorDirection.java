b61295ee7ca00813c24a4b7d53ed7a374d544f4a
Kaleb <dassou-ablam-kaleb.sika@etu-univ-cotedazur.fr>
2022-12-03 16:00:02 +0100

    Creation de VectorDirection Representant les vecteurs Unitaires Axiales des parcelles dans le plans

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/VectorDirection.java
@@ -0,0 +1,31 @@
+package fr.cotedazur.univ.polytech.startingpoint;
+
+public enum VectorDirection {
+    Q_UP(0, -1, +1),
+    Q_DOWN(0, +1, -1),
+    S_UP(+1, -1, 0),
+    S_DOWN(-1, +1, 0),
+    R_LEFT(-1, 0, +1),
+    R_RIGHT(+1, 0, -1);
+    private final int q;
+    private final int s;
+    private final int r;
+
+    VectorDirection(int q1, int s1, int r1) {
+        this.q = q1;
+        this.s = s1;
+        this.r = r1;
+    }
+
+    public int getQ() {
+        return q;
+    }
+
+    public int getS() {
+        return s;
+    }
+
+    public int getR() {
+        return r;
+    }
+}
