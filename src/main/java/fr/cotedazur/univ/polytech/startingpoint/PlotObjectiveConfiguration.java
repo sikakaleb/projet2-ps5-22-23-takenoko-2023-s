e9085bfee4eb68a875877f836c9a70e30cfa1745
Kaleb <dassou-ablam-kaleb.sika@etu-univ-cotedazur.fr>
2022-12-07 20:40:19 +0100

    Creation de PlotObjective.java

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/PlotObjectiveConfiguration.java
@@ -0,0 +1,35 @@
+package fr.cotedazur.univ.polytech.startingpoint;
+
+/**Enumeration de type de configuration parcelle
+ * Que nous pouvons aobtenir**/
+public enum PlotObjectiveConfiguration {
+
+    /*
+     * DIRECTSAMEPLOTS
+     * 3 Parcelle Alignée
+     *  #-#-#
+     */
+    DIRECTSAMEPLOTS,
+    /*
+     * INDIRECTSAMEPLOTS
+     * ressemblant au suivant mais 2 parcelle
+     * sont se
+     *  #-()-#
+     *    #
+     */
+    INDIRECTSAMEPLOTS,
+    /*
+     * INDIRECTSAMEPLOTS
+     * 3 Parcelle Alignée
+     *  #-#
+     *   #
+     */
+    TRIANGULARSAMEPLOTS,
+    /*
+     * INDIRECTSAMEPLOTS
+     * 3 Parcelle Alignée
+     *    #-#
+     *   #-#
+     */
+    QUADRILATERALSAMEPLOTS;
+}
