c0022bd097dbafd2ffcde61cc48fbfa9cdc5cf56
Kaleb <dassou-ablam-kaleb.sika@etu-univ-cotedazur.fr>
2022-12-07 20:42:22 +0100

    Creation de Objective.java

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Objective.java
@@ -0,0 +1,25 @@
+package fr.cotedazur.univ.polytech.startingpoint;
+
+public class Objective {
+    /**Attributs de la classe**/
+    public static  int NumberOfObjective=0;
+    private int objectId;
+    private  int NumberOfPoints;
+
+    /**COntructeur de la classe**/
+    public Objective(int numberOfPoints) {
+        objectId=++NumberOfObjective;
+        NumberOfPoints = numberOfPoints;
+
+    }
+
+    /**Acesseur de la classe*/
+
+    public int getObjectId() {
+        return objectId;
+    }
+
+    public int getNumberOfPoints() {
+        return NumberOfPoints;
+    }
+}
