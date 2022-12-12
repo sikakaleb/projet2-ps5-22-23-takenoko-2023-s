84fcd5bc48c021c5649335b9c694d0af72525cb2
Kaleb <dassou-ablam-kaleb.sika@etu-univ-cotedazur.fr>
2022-12-12 14:50:43 +0100

    Modification de PlotObjectiveConfiguration.java  ajout des 3 types de configuration QUADRILATERAL #21

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/PlotObjectiveConfiguration.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/PlotObjectiveConfiguration.java
@@ -26,10 +26,37 @@ public enum PlotObjectiveConfiguration {
      */
     TRIANGULARSAMEPLOTS,
     /*
-     * INDIRECTSAMEPLOTS
+     * QUADRILATERALSAMEPLOTS
      * 3 Parcelle Alignée
      *    #-#
      *   #-#
      */
-    QUADRILATERALSAMEPLOTS;
+    QUADRILATERALSAMEPLOTS,
+    /*
+     * P -->Parcelle de couleur rose (pink)
+     * Y -->Parcelle de couleur jaune (Yellow)
+     * QUADRILATERALSAMEPLOTS_P_Y PINK ET YELLOW
+     * 3 Parcelle Alignée
+     *    P-P
+     *   Y-Y
+     */
+    QUADRILATERALSAMEPLOTS_P_Y,
+    /*
+     * P -->Parcelle de couleur rose (pink)
+     * G -->Parcelle de couleur verte (Green)
+     * QUADRILATERALSAMEPLOTS_G_P PINK ET YELLOW
+     * 3 Parcelle Alignée
+     *    P-P
+     *   G-G
+     */
+    QUADRILATERALSAMEPLOTS_G_P,
+    /*
+     * P -->Parcelle de couleur rose (pink)
+     * Y -->Parcelle de couleur jaune (Yellow)
+     * QUADRILATERALSAMEPLOTS_P_Y PINK ET YELLOW
+     * 3 Parcelle Alignée
+     *    G-G
+     *   Y-Y
+     */
+    QUADRILATERALSAMEPLOTS_G_Y;
 }
