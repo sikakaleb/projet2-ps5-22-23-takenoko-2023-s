606820a8f4d262c3f2dc913bbd0fea8b31426718
komi-assimpah <komi-jean-paul.assimpah@etu.unice.fr>
2023-01-11 09:14:33 +0100

    Ajout de classe irrigation

---
+++ src/main/java/supplies/Irrigation.java
@@ -0,0 +1,18 @@
+package supplies;
+
+public class Irrigation {
+    private HexPlot plot1;
+    private HexPlot plot2;
+
+
+    public Irrigation (HexPlot plot1, HexPlot plot2){
+        this.plot1=plot1;
+        this.plot2=plot2;
+    }
+
+
+
+
+
+
+}
