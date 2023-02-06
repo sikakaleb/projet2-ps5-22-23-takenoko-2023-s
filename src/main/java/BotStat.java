05c890a720c82b925786a9d3646bcaccfb625047
Kaleb <dassou-ablam-kaleb.sika@etu-univ-cotedazur.fr>
2023-02-06 23:15:10 +0100

    ajout de bot statistic #55

---
+++ src/main/java/BotStat.java
@@ -0,0 +1,38 @@
+class BotStat {
+    private String botName;
+    private int gamesPlayed;
+    private int wins;
+    private int losses;
+
+    public String getBotName() {
+        return botName;
+    }
+
+    public void setBotName(String botName) {
+        this.botName = botName;
+    }
+
+    public int getGamesPlayed() {
+        return gamesPlayed;
+    }
+
+    public void setGamesPlayed(int gamesPlayed) {
+        this.gamesPlayed = gamesPlayed;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public void setLosses(int losses) {
+        this.losses = losses;
+    }
+}
---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/BotStatistics.java
@@ -0,0 +1,44 @@
+package fr.cotedazur.univ.polytech.startingpoint;
+
+import com.opencsv.CSVWriter;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class BotStatistics {
+
+    private static final String STATS_DIRECTORY = "stats";
+    private static final String STATS_FILE = "gamestats.csv";
+    private static final String FILE_LOCATION = Paths.get(STATS_DIRECTORY, STATS_FILE).toString();
+
+    public void saveStatistics(List<BotStat> stats) {
+        File file = new File(FILE_LOCATION);
+
+        try (CSVWriter writer = new CSVWriter(new FileWriter(file, true))) {
+            for (BotStat stat : stats) {
+                writer.writeNext(new String[] { stat.getBotName(), String.valueOf(stat.getGamesPlayed()),
+                        String.valueOf(stat.getWins()), String.valueOf(stat.getLosses()) });
+            }
+        } catch (IOException e) {
+            System.err.println("An error occurred while saving statistics to file: " + e.getMessage());
+        }
+    }
+
+    public List<BotStat> loadStatistics() {
+        File file = new File(FILE_LOCATION);
+
+        try {
+            CsvToBean<BotStat> csvToBean = new CsvToBeanBuilder<BotStat>(new FileReader(file))
+                    .withType(BotStat.class).build();
+            return csvToBean.parse();
+        } catch (IOException e) {
+            System.err.println("An error occurred while loading statistics from file: " + e.getMessage());
+            return null;
+        }
+    }
+}
