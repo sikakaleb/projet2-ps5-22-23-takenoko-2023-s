f4380f119125e8ba4714b394b24b7154ae05f60b
ChatelainClement <clementchatelain@hotmail.fr>
2023-02-08 16:26:13 +0100

    Modification de lancement d'une partie avec le jcommander suite a un bug qui lancé 1000 fois la meme partie #61

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/gameplay/Engine.java
@@ -0,0 +1,113 @@
+package fr.cotedazur.univ.polytech.startingpoint.gameplay;
+
+import fr.cotedazur.univ.polytech.startingpoint.display.Display;
+import fr.cotedazur.univ.polytech.startingpoint.supplies.Emperor;
+
+import java.util.List;
+import java.util.Map;
+
+import static fr.cotedazur.univ.polytech.startingpoint.gameplay.Main.*;
+import static fr.cotedazur.univ.polytech.startingpoint.tools.Action.GameAction.MOVE_GARDENER;
+import static fr.cotedazur.univ.polytech.startingpoint.tools.Action.GameAction.MOVE_PANDA;
+import static fr.cotedazur.univ.polytech.startingpoint.tools.Strategy.*;
+
+public class Engine {
+
+
+    public Player p1;
+    public Player p2;
+
+    Map<Integer, Integer> objectivesForNbPlayers = Map.of(
+            2, 9,
+            3, 8,
+            4, 7
+    );
+    private int nbObjectivesToWin;
+
+    // Dans notre version, avec des bots peu intelligents,pour éviter que la partie
+    // ne soit interminable, on fixe nombre de tours prédéterminé :
+    public int maxRounds = 50;
+    public int nbRound = 0;
+
+    public Engine(Player p1, Player p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    /*
+     * JeReflechis() utilisé pour marquer un temps de pause
+     * la transition entre les tours de jeu de chaque joueurs
+     */
+    public static void  jeReflechis() {
+        try {
+            for (int i = 0; i < 6; i++) {
+                Thread.sleep(6);
+            }
+        }catch(Exception e) {
+            Display.printMessage( String.valueOf(e));
+        }
+    }
+
+    public void runGame(Game game, boolean stats){
+        Boolean loop = true, lastRound = false;
+        Emperor emperor = new Emperor(game);
+        nbObjectivesToWin = objectivesForNbPlayers.get(game.getPlayerList().size());
+        List<Player> playerList = game.getPlayerList();
+
+        Display.printMessage("---------------BEGIN----------------");
+        while (loop && nbRound < maxRounds){
+
+            loop = !lastRound;
+
+            for(Player p : playerList ){
+                Display.printMessage("");
+
+                if (p.getObjectiveAchieved().size() == nbObjectivesToWin) {
+                    Display.printMessage( "Dernier tour ! "+p.getName()+" a valide "+nbObjectivesToWin+" objectifs.");
+                    p.pickEmperor();
+                    lastRound = true;
+                }
+
+                Display.printMessage("C'est le tour de : " + p.getName());
+                if (game.play(p)) game.display();
+
+                if (nbRound==0  && p.getStrategy()==Fa3STRATEGY){
+                    Display.printMessage(p.getStrategy().getActions().toString());
+                    p.getStrategy().add(MOVE_GARDENER);
+                    p.getStrategy().add(MOVE_PANDA);
+                }
+
+            }
+            nbRound++;
+        }
+        if (nbRound == maxRounds)
+            Display.printMessage("Le jeu se termine au bout de "+nbRound+" tours.");
+
+        Player winner = emperor.judgement();
+        System.out.println("Score joueur 1 : " + p1.getScore());
+        System.out.println("Score joueur 2 : " + p2.getScore());
+
+        if (stats) {
+            for(Player p : playerList){
+                gameStats.get(p).played();
+            }
+            if (winner != null) {
+                gameStats.get(winner).win();
+            } else {
+                for (Player p : playerList) {
+                    gameStats.get(p).tie();
+                }
+            }
+
+            gameStats.get(p1).score(p1.getScore());
+            gameStats.get(p2).score(p2.getScore());
+
+            gameStats.get(p1).updateAverageScore();
+            gameStats.get(p2).updateAverageScore();
+
+
+
+        }
+    }
+
+}
--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/gameplay/Main.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/gameplay/Main.java
@@ -4,17 +4,16 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import fr.cotedazur.univ.polytech.startingpoint.data.BotStat;
 import fr.cotedazur.univ.polytech.startingpoint.data.BotStatistics;
-import fr.cotedazur.univ.polytech.startingpoint.data.PlayerData;
 import fr.cotedazur.univ.polytech.startingpoint.display.Display;
-import fr.cotedazur.univ.polytech.startingpoint.supplies.Emperor;
+import fr.cotedazur.univ.polytech.startingpoint.tools.Strategy;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.stream.IntStream;
 
-import static fr.cotedazur.univ.polytech.startingpoint.tools.Action.GameAction.*;
 import static fr.cotedazur.univ.polytech.startingpoint.tools.Strategy.*;
 
 public class Main {
@@ -28,37 +27,11 @@ public class Main {
     @Parameter(names = "--csv", description = "Simulation à plusieurs parties avec relecture de \"stats/gamestats.csv\" s’il existe et ajout des nouvelles statistiques")
     private static boolean csv;
 
-    private static Player p1 = new Player("BotIntelligent", PANDASTRATEGY);
-    private static Player p2 = new Player("BotRandom", WITHOUTSTRATEGY);
-    public static Game game = new Game(p1,p2);
-    private static Map<Player, PlayerData> gameStats;
     public static int ITERATIONS = 1000;
-    public static int ties = 0;
-    Map<Integer, Integer> objectivesForNbPlayers = Map.of(
-            2, 9,
-            3, 8,
-            4, 7
-    );
-    private int nbObjectivesToWin = objectivesForNbPlayers.get(game.getPlayerList().size());
-
-    // Dans notre version, avec des bots peu intelligents,pour éviter que la partie
-    // ne soit interminable, on fixe nombre de tours prédéterminé :
-    public int maxRounds = 50;
-    public int nbRound = 0;
-
-    /*
-    * JeReflechis() utilisé pour marquer un temps de pause
-    * la transition entre les tours de jeu de chaque joueurs
-    */
-    public static void  jeReflechis() {
-        try {
-            for (int i = 0; i < 6; i++) {
-                Thread.sleep(6);
-            }
-        }catch(Exception e) {
-            Display.printMessage( String.valueOf(e));
-        }
-    }
+    public static Map<Player, BotStat> gameStats;
+    Player p1;
+    Player p2;
+    public static List<Player> playerList = new ArrayList<>();
 
     /*
      * Main dans lequel se trouve une similation du jeu entre 2 joueurs
@@ -71,92 +44,62 @@ public class Main {
                 .addObject(main)
                 .build()
                 .parse(argv);
-
-        if (demo) {
-            Display.setUp(Level.INFO);
-            main.runGame();
+        main.run();
+    }
+    public void run() {
+        if (twothousand) {
+            twoThousand();
+        } else if (demo) {
+            demo();
+        } else if (csv) {
+            csv();
+        } else {
+            Display.printMessage("Aucun argument n'a été passé en paramètre");
         }
+    }
 
-        else if (twothousand) {
-            Display.setUp(Level.SEVERE);
+    public void demo() {
+        Display.setUp(Level.INFO);
+        Engine engine = new Engine(new Player("BotIntelligent", PANDASTRATEGY), new Player("BotRandom", WITHOUTSTRATEGY));
+        engine.runGame(new Game(engine.p1, engine.p2),false);
+    }
 
-            Display.printMessage("Simulation de "+ITERATIONS+" parties de votre meilleur bot contre le second", Level.SEVERE);
-            ties = 0;
-            gameStats = Map.of(p1, new PlayerData(), p2, new PlayerData());
-            IntStream.range(0, ITERATIONS).forEach(i -> main.runGame());
-            Display.printGameStats(game.playerList, gameStats);
-
-            Display.printMessage("\nSimulation de "+ITERATIONS+" parties de votre meilleur bot contre lui-même", Level.SEVERE);
-            p2.setStrategy(PANDASTRATEGY);
-            ties = 0;
-            gameStats = Map.of(p1, new PlayerData(), p2, new PlayerData());
-            IntStream.range(0, ITERATIONS).forEach(i -> main.runGame());
-            Display.printGameStats(game.playerList, gameStats);
-        }
-
-        else if(csv){
-            Display.setUp(Level.SEVERE);
-            Display.printMessage("Simulation de " + ITERATIONS +" parties avec relecture de \"stats/gamestats.csv\" s’il existe et ajout des nouvelles statistiques", Level.SEVERE);
-            ties = 0;
-            gameStats = Map.of(p1, new PlayerData(), p2, new PlayerData());
-            IntStream.range(0, ITERATIONS).forEach(i -> main.runGame());
-            Display.printGameStats(game.playerList, gameStats);
-            List<BotStat> botStats = Arrays.asList(
-                    new BotStat(p1, ITERATIONS, gameStats.get(p1).getWins(), gameStats.get(p1).getLosses()),
-                    new BotStat(p2, ITERATIONS, gameStats.get(p2).getWins(), gameStats.get(p2).getLosses())
-            );
-            List<BotStat> existingStats = BotStatistics.readFromFile();
-            Display.printMessage(String.valueOf(existingStats.size()), Level.SEVERE);
-            existingStats.addAll(botStats);
-            BotStatistics.writeToFile(existingStats);
-        }
+    public void csv() {
+        Display.setUp(Level.SEVERE);
+        Display.printMessage("Simulation de " + ITERATIONS +" parties avec relecture de \"stats/gamestats.csv\" s'il existe et ajout des nouvelles statistiques", Level.SEVERE);
+        gameWithStats(PANDASTRATEGY,WITHOUTSTRATEGY);
+        Display.printGameStats(playerList, gameStats);
+        List<BotStat> botStats = Arrays.asList(
+                new BotStat("p1", PANDASTRATEGY,gameStats.get(p1).getGamesPlayed(), gameStats.get(p1).getWins(), gameStats.get(p1).getTies(), gameStats.get(p1).getLosses(), gameStats.get(p1).getPoints()),
+                new BotStat("p2", WITHOUTSTRATEGY, gameStats.get(p2).getGamesPlayed(), gameStats.get(p2).getWins(), gameStats.get(p2).getTies(), gameStats.get(p2).getLosses(), gameStats.get(p2).getPoints())
+        );
+        List<BotStat> existingStats = BotStatistics.readFromFile();
+        Display.printMessage(String.valueOf(existingStats.size()), Level.SEVERE);
+        existingStats.addAll(botStats);
+        BotStatistics.writeToFile(botStats);
 
     }
 
-    public void runGame(){
-        Boolean loop = true, lastRound = false;
-        Emperor emperor = new Emperor(game);
-        List<Player> playerList = game.getPlayerList();
+    public void twoThousand() {
+        Display.setUp(Level.SEVERE);
 
-        Display.printMessage("---------------BEGIN----------------");
-        while (loop && nbRound < maxRounds){
+        Display.printMessage("Simulation de "+ITERATIONS+" parties de votre meilleur bot contre le second", Level.SEVERE);
+        gameWithStats(PANDASTRATEGY,WITHOUTSTRATEGY);
 
-            loop = !lastRound;
-
-            for(Player p : playerList ){
-                Display.printMessage("");
-
-                if (p.getObjectiveAchieved().size() == nbObjectivesToWin) {
-                    Display.printMessage( "Dernier tour ! "+p.getName()+" a valide "+nbObjectivesToWin+" objectifs.");
-                    p.pickEmperor();
-                    lastRound = true;
-                }
-
-                Display.printMessage("C'est le tour de : " + p.getName());
-                if (demo) jeReflechis();
-                if (game.play(p)) game.display();
-
-                if (nbRound==0  && p.getStrategy()==Fa3STRATEGY){
-                    System.out.println(p.getStrategy().getActions());
-                    p.getStrategy().add(MOVE_GARDENER);
-                    p.getStrategy().add(MOVE_PANDA);
-                }
-
-            }
-            nbRound++;
-        }
-        if (nbRound == maxRounds)
-            Display.printMessage("Le jeu se termine au bout de "+nbRound+" tours.");
-
-        Player winner = emperor.judgement();
-        if (twothousand || csv) {
-            if (winner != null) {
-                gameStats.get(winner).win();
-            } else {
-                ties++;
-            }
-            gameStats.get(p1).score(p1.getScore());
-            gameStats.get(p2).score(p2.getScore());
-        }
+        Display.printMessage("\nSimulation de "+ITERATIONS+" parties de votre meilleur bot contre lui-même", Level.SEVERE);
+        gameWithStats(PANDASTRATEGY,PANDASTRATEGY);
     }
+
+    private void gameWithStats(Strategy strategy1, Strategy strategy2) {
+        gameStats = Map.of(p1, new BotStat(), p2, new BotStat());
+        IntStream.range(0, ITERATIONS).forEach(i -> {
+            Display.printMessage("Partie "+(i+1), Level.SEVERE);
+            Engine engine = new Engine(new Player("BotIntelligent", strategy1), new Player("BotRandom", strategy2));
+            engine.runGame(new Game(engine.p1,engine.p2),true);
+        });
+        playerList.add(p1);
+        playerList.add(p2);
+        Display.printGameStats(playerList, gameStats);
+    }
+
 }
